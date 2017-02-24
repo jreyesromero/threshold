70a41483dcf7f8f131c1b998a20b2a0d64aa9f92
Julian Reyes Romero <julian.reyes.romero@ericsson.com>
2017-02-24 10:43:59 +0100

    Included new functions to get time in seconds

--- threshold.py
+++ threshold.py
@@ -39,31 +39,24 @@ def treat_log_file(log_file, threshold_file):
     sys.exit(1)
 
   for line in lfile.readlines():
+    
     read_line = line.split()
     phase, procedure, time = read_line[0], read_line[1], read_line[2]
-#        print("Phase name: {} ".format(phase))
-#        print("Procedure name: {}".format(procedure))
-#        print("Time: {}".format(time))
     threshold_time = read_threshold_json_file(threshold_file, phase, procedure)
+
     if threshold_time is None:
-      print("Run time for process not found for phase {} proc {}".format(phase,procedure))
+      print("Run time for phase {} and proc {} NOT FOUND\n".format(phase,procedure))
     else:
-      print("RUNTIME {} threshold {}".format(time,threshold_time))
-#      check_run_time(time, threshold_time)
+      print("phase: {} procedure: {} process_runtime: {} threshold: {}".format(phase,procedure,time,threshold_time))
       if diff_times_in_seconds(time, threshold_time):
-        print("Difference inside 10 percent allowed")
+        print("  Difference inside 10 percent ALLOWED\n")
       else:
-        print("percentage difference excedes 10 percent allowed")
+        print("  percentage difference excedes 10 percent allowed\n")
 
 def diff_times_in_seconds(process_run_time, process_threshold_time):
-  time1 = datetime.strptime(process_run_time,"%H:%M:%S")
-  time2 = datetime.strptime(process_threshold_time,"%H:%M:%S")
-  h1, m1, s1 = time1.hour, time1.minute, time1.second
-  h2, m2, s2 = time2.hour, time2.minute, time2.second
-  t1_secs = s1 + 60 * (m1 + 60*h1)
-  t2_secs = s2 + 60 * (m2 + 60*h2)
-  print("run_time_seconds {} threshold_seconds {}".format(t1_secs, t2_secs))
-  result_secs = ( t1_secs - t2_secs)
+  t1_secs = convert_string_to_seconds(process_run_time)
+  t2_secs = convert_string_to_seconds(process_threshold_time)
+  result_secs = (t1_secs - t2_secs)
 
   # if result_secs > 0 : process_run_time higher than threshold
   # if result_secs < 0 : process_run_time less than threshold
@@ -76,13 +69,16 @@ def diff_times_in_seconds(process_run_time, process_threshold_time):
 
   return True
 
+def convert_string_to_seconds(input_time):
+  time = datetime.strptime(input_time,"%H:%M:%S")
+  return get_time_in_seconds(time)
+
+
+def get_time_in_seconds(time):
+  secs = time.second + 60 * (time.minute + 60*time.hour)
+  return secs
+
 if __name__ == '__main__':
     options, args = parse_arguments()
-#    run_time = read_threshold_json_file(options.threshold, 'SV', 'PROC_SYSTEM_UPGRADE_PREPARATION')
-
-#    for descriptor in phases:
-#        phase_name = descriptor.get("name")
-#        procedures = descriptor.get("procedures",[])
-
     treat_log_file(options.log_file, options.threshold)
         
